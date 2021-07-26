d57e596b5d0644bc878618f3c2357d385da01ecd
Douglas Fugita <douglas.cezar@gmail.com>
2021-07-26 11:58:40 -0300

    Specification Pattern

---
+++ NStoreEnterprise/src/buildingblocks/NStore.Core/Specification/AndSpecification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace NStore.Core.Specification
+{
+    internal class AndSpecification<T> : Specification<T>
+    {
+        private Specification<T> _left;
+        private Specification<T> _right;
+
+        public AndSpecification(Specification<T> left, Specification<T> right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var leftExpression = _left.ToExpression();
+            var rightExpression = _right.ToExpression();
+
+            var invokedExpression = Expression.Invoke(rightExpression, leftExpression.Parameters);
+
+            return (Expression<Func<T, bool>>)Expression.Lambda(Expression.AndAlso(leftExpression.Body, invokedExpression), leftExpression.Parameters);
+        }
+    }
+}
---
+++ NStoreEnterprise/src/buildingblocks/NStore.Core/Specification/OrSpecification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace NStore.Core.Specification
+{
+    internal class OrSpecification<T> : Specification<T>
+    {
+        private Specification<T> _left;
+        private Specification<T> _right;
+
+        public OrSpecification(Specification<T> left, Specification<T> right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var leftExpression = _left.ToExpression();
+            var rightExpression = _right.ToExpression();
+
+            var invokedExpression = Expression.Invoke(rightExpression, leftExpression.Parameters);
+
+            return (Expression<Func<T, bool>>)Expression.Lambda(Expression.OrElse(leftExpression.Body, invokedExpression), leftExpression.Parameters);
+        }
+    }
+}
